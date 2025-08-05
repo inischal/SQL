3871473146304f7c8656500b529502f3c63e6c20
inischal <nspnischal@gmail.com>
2025-08-05 18:48:06 +0545

    Two Users data added/Updated

---
+++ DML.sql
@@ -0,0 +1,9 @@
+/* Data Manipulation (DML)*/
+use MyDatabase;
+
+select * from customers;
+
+INSERT INTO customers (id, first_name, country, score)
+VALUES 
+	(6, 'Anna', 'USA',NULL),
+	(7,'Sam',NULL, 100);
