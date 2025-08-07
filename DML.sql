f46b42678d349a09b6ed0a73286a99debe3ead07
inischal <nspnischal@gmail.com>
2025-08-07 12:32:03 +0545

    copied data from customers table to persons table

--- DML.sql
+++ DML.sql
@@ -12,3 +12,22 @@ VALUES
 INSERT INTO customers (id, first_name)
 VALUES
 	(8,'Andreas');
+
+
+/* Copy data from 'customers' table into 'persons' */
+CREATE table persons(
+	id int NOT NULL PRIMARY KEY,
+	person_name varchar(50) NOT NULL,
+	birth_date DATE NULL,
+	phone varchar(15) NOT NULL
+);
+INSERT INTO persons(id, person_name, birth_date, phone)
+SELECT
+id,
+first_name,
+NULL,
+'Unknown'
+from customers
+select * from persons;
+
+
